c905ea96282e64bd5834971bfe5640bdb3ed6fc7
MyascoFP <ivansavchuk20011@gmail.com>
2024-06-26 00:56:34 +0300

    add link to git

--- pages/main.py
+++ pages/main.py
@@ -9,7 +9,7 @@ layout = dbc.Container([
             html.H1("О проекте"),
             html.P("Этот проект предназначен для анализа университетов по различным критериям, включая мировой рейтинг, академическую репутацию, цитирования и др."),
             html.P("Для просмотра исходного кода проекта, посетите наш репозиторий на GitHub:"),
-            html.A("GitHub репозиторий", href="https://github.com/your_username/your_repository"),
+            html.A("GitHub репозиторий", href="https://github.com/MyascoFP/university_project_backup"),
             html.Hr(style={'color': 'black'}),
         ], style={'textAlign': 'center'})
     ]),
